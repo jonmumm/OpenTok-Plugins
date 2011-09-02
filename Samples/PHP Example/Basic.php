ef7d51bee102ca848e30f7703cfbb258dce92757
Jonathan Mumm <mumm@tokbox.com>
2011-09-01 18:52:58 -0700

    Added logic to create token based on privileges

--- Samples/PHP Example/Basic.php
+++ Samples/PHP Example/Basic.php
@@ -16,10 +16,19 @@
 
   $apiObj = new OpenTokSDK(API_Config::API_KEY, API_Config::API_SECRET);
 
-  // Use GenerateSession.php to generate a sessionId to use for everybody
-  // Alternatively you could generate sessions here and store them in your database
+  // In this case this sessionId is pre-generated and inserted here.
+  // Alternatively you could generate one and store it in your database.
   $sessionId = '142d24bb826a0e65ecd4c93b57d0d99f8c4fb3ce'; // Insert your sessionId
 
+  // Some logic here to determine who is a publisher
+  $moderator = true;
+
+  // Generate token depending on user permissions
+  if ($moderator) {
+    $token = $apiObj->generate_token($sessionId, RoleConstants::PUBLISHER); 
+  } else {
+    $token = $apiObj->generate_token($sessionId, RoleConstants::SUBSCRIBER);
+  }
 ?>
 	<div id='widgetContainer' style='background: #222'></div>
 	
@@ -28,9 +37,11 @@
 		// Set OpenTok variables
     var session = TB.initSession('<?php print $sessionId; ?>');
     var api_key = '<?php print API_Config::API_KEY; ?>';
-    var token = '<?php print $apiObj->generate_token($sessionId); ?>';
+    var token = '<?php print $token; ?>';
 
 		var widget = _OT.widget;
+
+    TB.setLogLevel(TB.DEBUG);
 		
 		// Initialize the widget and pass it properties
 		widget.init(session, 'widgetContainer', { width: '500', height: '400', publisherSize: 'medium', name: 'OpenTok User 123', publishFull: false });
